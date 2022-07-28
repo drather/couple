4d83bc3cc9a6884d70b8f4ef7352bba2ca4c8a94
김경석 <drather_dev@naver.com>
2022-07-28 11:50:04 +0900

    feat: MyType - Member 일대일 관계 설정

--- src/main/java/com/maker/couple/domain/MyType.java
+++ src/main/java/com/maker/couple/domain/MyType.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @AllArgsConstructor
@@ -30,5 +27,7 @@ public class MyType {
 
     private int higherSalary;
 
-
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
 }
